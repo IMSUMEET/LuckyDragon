af7b50d62a547287e7b3836b7ae00bcffc89b135
Sumeet Suryawanshi <S.l.suryawanshi4@gmail.com>
2021-08-11 18:34:46 -0700

    complete coin toss code

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+
+
+// Toss Part --------------------------------------------------------------------
+
+var isStarted = false;
+
 $("#toss-button").click(function() {
-	alert("Hello World");
+	if(isStarted === false) {
+		var result = Math.floor((Math.random()*2));
+		var resultImage ;
+		if(result === 0){
+			resultImage =  "images/heads.jpg";
+		}else {
+			resultImage = "images/tails.jpg";
+		}
+		$("#toss-image").attr("src" , resultImage);
+		$("#toss-image").addClass("centralize-toss");
+		isStarted = true;
+	}else{
+		console.log("Game is already started");
+	}
+	
 })
